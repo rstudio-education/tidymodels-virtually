c7b77a6fa7fb8d774e67af0fd59d665a89ff130f
Alison Hill <apreshill@gmail.com>
2021-09-22 13:29:01 -0700

    update site

--- site/materials/rmed-backup/04-case-study/knn.R
+++ site/materials/rmed-backup/04-case-study/knn.R
@@ -52,5 +52,7 @@ knn_workflow <-
 knn_results <- 
   knn_workflow %>% 
   fit_resamples(resamples = val_set,
-                control = control_resamples(save_pred = TRUE),
+                # change me to control_grid with tune_grid
+                control = control_resamples(save_pred = TRUE,
+                                            verbose = TRUE),
                 metrics = metric_set(roc_auc))
